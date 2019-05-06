bc71c39436f8cd9741dacacdafcc6a21383076da
ChristopheVdE <46569386+ChristopheVdE@users.noreply.github.com>
2019-05-06 12:10:03 +0200

    SPAdes added

--- Docker/01-Bash/04_copy_results.sh
+++ Docker/01-Bash/04_copy_results.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 
 ###################################################################################
-#NAME SCRIPT: 01_copy_rawdata.sh
+#NAME SCRIPT: 04_copy_results.sh
 #AUTHOR: Christophe Van den Eynde  
 #copying rawdata to the current analysis data folders
-#USAGE: ./01_copy_rawdata.sh ${input} ${output}
+#USAGE: ./04_copy_results.sh ${input} ${output}
 ###################################################################################
 
 # copy the rawdata-files to the 'current analysis data-folder'
 echo -e "\nCopying files, please wait"
-    # put the rawdata into the correct file-structure 
-mv /home/rawdata/* /home/rawdata/00_Rawdata/
-    # copy the 00_Rawdata into the current analysis folder
-mkdir -p /home/Pipeline/data/00_Rawdata
-cp -vr /home/rawdata/00_Rawdata/ /home/Pipeline/data/00_Rawdata/
+# copy the 00_Rawdata into the current analysis folder
+cp -vrn /home/Pipeline/data/* /home/rawdata/00_Rawdata/ 
 echo -e "Done\n"
---
+++ Snakemake_Linux.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+###################################################################################
+#NAME SCRIPT: Snakemake_Linux.sh
+#AUTHOR: Christophe Van den Eynde  
+#specifies the number of threads to use and executes the snakemake command
+#USAGE: ./Snakemake_Linux.sh
+###################################################################################
+
+# Counting treads
+Treads=`nproc --all`
+# specifying the number of threads snakemake can use
+if ${Treads} -le 4; then 
+    s_Treads=$((${Treads}/2))
+else
+    s_Treads=$((${Treads}/4*3))
+fi 
+
+echo "${Treads} treads found, reserving ${s_Treads} treads for snakemake"
+
+# Starting snakemake
+echo "\nStarting snakemake"
+snakemake -j ${Treads}
+    # -j = how many cores can be used at most in parallel 
+echo "Done\n"
