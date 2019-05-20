d6e3674e58e646c6103bba943cb9ca35de30de8f
ChristopheVdE <46569386+ChristopheVdE@users.noreply.github.com>
2019-05-20 12:30:05 +0200

    change get_environment version in the windows start-up script

--- WINDOWS_run-pipeline.cmd
+++ WINDOWS_run-pipeline.cmd
@@ -1,7 +1,7 @@
 @echo off
 
 REM RUN SNAKEMAKE.txt-----------------------------------------------
-python.exe get_environment_v1.1.py
+python.exe get_environment_v2.0.py
 REM ----------------------------------------------------------------
 
 PAUSE
