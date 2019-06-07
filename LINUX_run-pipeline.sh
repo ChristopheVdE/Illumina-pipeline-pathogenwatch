e44139fae146cd6aedfc0a67b731a51cdc70c290
ChristopheVdE <46569386+ChristopheVdE@users.noreply.github.com>
2019-06-07 11:05:05 +0200

    versioning

--- LINUX_run-pipeline.sh
+++ LINUX_run-pipeline.sh
@@ -10,6 +10,6 @@
 # Starting snakemake
 echo "\nStarting snakemake"
 cd "$(dirname "$BASH_SOURCE")"
-python3 ./get_environment_v2.3.py
+python3 ./get_environment_v2.4.py
 echo "done"
 # echo "Done\n"
--- WINDOWS_run-pipeline.cmd
+++ WINDOWS_run-pipeline.cmd
@@ -1,7 +1,7 @@
 @echo off
 
 REM RUN SNAKEMAKE.txt-----------------------------------------------
-python.exe get_environment_v2.3.py
+python.exe get_environment_v2.4.py
 REM ----------------------------------------------------------------
 
 PAUSE
